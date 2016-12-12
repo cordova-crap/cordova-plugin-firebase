50035fc0a157281789824771c5764212ebb65b24
cordova-crap <rsdifu+cordova-crap@gmail.com>
2016-12-12 18:30:05 +0000

    iOS 10.x check support testing

--- src/ios/AppDelegate+FirebasePlugin.m
+++ src/ios/AppDelegate+FirebasePlugin.m
@@ -106,7 +106,7 @@
     [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
 }
 
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+#if (defined(__IPHONE_10_0) || defined(__IPHONE_10_1)) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
        willPresentNotification:(UNNotification *)notification
          withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
